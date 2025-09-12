ae6ec1d11e3f909f3fb861297460180f2c6494c9
amjvidal <antoniomariojesus123@gmail.com>
2025-09-11 23:36:25 -0300

    Atividade 3

---
+++ atv3/CalcArea.java
@@ -0,0 +1,10 @@
+public class CalcArea {
+    static int calcArea (int height, int width) {
+        return height*width;
+
+    }
+    public static void main(String[] args) {
+        byte g = calcArea(4, 20);
+        System.out.println(g);
+    }
+}
---
+++ atv3/Clock.java
@@ -0,0 +1,11 @@
+public class Clock {
+    String time;
+
+    void setTime(String t){
+        time = t;
+    }
+
+    void getTime(){
+        return time;
+    }
+}
